f53aab16606aa74f8f33161082f9fb0eaef57253
jackwigney <jack.wigney@gmail.com>
2023-10-03 19:56:29 +1100

    HW Week 10

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var vg_1 = "idioms/sphere.json";
+var vg_2 = "idioms/industry_count.json"
 
 vegaEmbed("#map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#count", vg_2).then(function(result){
+
 }).catch(console.error);
